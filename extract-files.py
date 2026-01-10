a5e5a73b45ef3c6ae3d3b9df24a393d2ed7aa7a6
Shirayuki39 <lorddemecrius83@proton.me>
2026-01-11 04:19:55 +0800

    X6882: Stop patching audio hw module with lib{binder,utils,hidlbase}-v32
    * We likely don't need to do this at all
    
    Change-Id: Ifd59674fde1633c953466c7bba839a0cfbe2618f
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
    Signed-off-by: jvaswb <bpsixta@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -50,10 +50,7 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')
-        .replace_needed('libalsautils.so', 'libalsautils-v31.so')
-        .replace_needed('libutils.so', 'libutils-v32.so')
-        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so')
-        .replace_needed('libbinder.so', 'libbinder-v32.so'),
+        .replace_needed('libalsautils.so', 'libalsautils-v31.so'),
     (
         'vendor/lib64/hw/sensors.mediatek.V2.0.so',
         'vendor/lib64/libcodec2_mtk_c2store.so',
