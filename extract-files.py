43642795e8b0be31e3cf6930ed54537f5ecaf6cf
putraaxzy <wibowo.yunanto41@smk.belajar.id>
2026-02-19 14:16:29 +0700

    X6882: wrap thermal_core to prevent SIGSEGV on missing cooler
    signal 11 (SIGSEGV), code 1 (SEGV_MAPERR), fault addr 0x0
      #00 pc 00000000000224a8 thermal_core (cpu_table_cooler+592)
      #01 pc 0000000000008d18 thermal_core (algorithm_thread+1384)
    
    Caused by cpu_table_mapping cooling device not present in sysfs.
    Add wrapper to check cooler availability before exec, override
    vendor service with stock trigger, and fix related sepolicy.

--- extract-files.py
+++ extract-files.py
@@ -59,8 +59,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('android.hardware.security.rkp-V3-ndk.so'),
     'vendor/etc/init/android.hardware.neuralnetworks-shim-service-mtk.rc': blob_fixup()
         .regex_replace('start', 'enable'),
-    'vendor/etc/init/init.thermal_core.rc': blob_fixup()
-        .regex_replace('ro.vendor.mtk_thermal_2_0', 'vendor.thermal.link_ready'),
     'vendor/etc/vintf/manifest/manifest_media_c2_V1_2_default.xml': blob_fixup()
         .regex_replace('1.1', '1.2'),
     ('vendor/lib64/mt6789/libneuralnetworks_sl_driver_mtk_prebuilt.so', 'vendor/lib64/libstfactory-vendor.so', 'vendor/lib64/libnvram.so', 'vendor/lib/libsysenv.so', 'vendor/lib64/libsysenv.so', 'vendor/lib64/libtflite_mtk.so'): blob_fixup()
