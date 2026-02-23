7b563437e461f332a7d527c772155fa3a3d522a0
Shirayuki39 <lorddemecrius83@proton.me>
2026-02-23 12:51:36 +0800

    Partially revert "X6882: Switch to common Mediatek IMS"
    This partially reverts commit 356f7e6ed187a324578c2af95ec52564daceded7.
    
    * Switching back to OEM MTK IMS, but keep inheriting Makefile as we commonized those to make our lives easier
    
    Refs:
    [1]: https://github.com/MillenniumOSS/android_vendor_mediatek_ims/tree/sixteen-oem
    
    Change-Id: I63392a64781a1ea0908f3c4d9a96b28bfbdcd6cf
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>
    Signed-off-by: zaidannn7 <72336864+zaidannn7@users.noreply.github.com>
    Signed-off-by: jvaswb <bpsixta@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -26,8 +26,16 @@ namespace_imports = [
     'hardware/transsion',
 ]
 
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
 lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
+    (
+        'libtflite_mtk',
+        'libneuron_graph_delegate.mtk',
+        'vendor.mediatek.hardware.videotelephony@1.0'
+    ): lib_fixup_vendor_suffix,
 }
 
 blob_fixups: blob_fixups_user_type = {
