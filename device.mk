eb82d4a2851acc2192be7c351dedf6a43c960297
luk1337 <priv.luk@gmail.com>
2026-06-13 21:26:40 +0700

    X6882: overlay: Configure aux camera for Aperture   *also set to 1080@60fps
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -317,6 +317,9 @@ PRODUCT_PACKAGES += \
     SystemUIResTarget \
     OpenDeltaOverlayMT6789
 
+PRODUCT_PACKAGES += \
+    ApertureOverlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
