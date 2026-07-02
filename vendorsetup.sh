d3a18d5366507ec147cfa86064121d35f31990a0
Shirayuki39 <lorddemecrius83@proton.me>
2026-07-02 09:54:31 +0800

    X6882: vendorsetup: Autopatch ROMs for Fenrir
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
-
 RET=0
+
+echo "- Applying fenrir compatiblity patches"
+cd system/core
+curl https://raw.githubusercontent.com/MillenniumOSS/patches/refs/heads/sixteen/system/core/0001-libfs_avb-Allow-LKs-patched-with-fenrir-to-boot-on-A.patch | git am || {
+  RET=1
+  git am --abort >/dev/null 2>&1
+}
+curl https://raw.githubusercontent.com/MillenniumOSS/patches/refs/heads/sixteen/system/core/0002-fastbootd-Always-return-false-for-GetDeviceLockStatu.patch | git am || {
+  RET=1
+  git am --abort >/dev/null 2>&1
+}
+cd ../../
+
 echo "- Applying Aperture Mediatek HFPS Mode"
 cd packages/apps/Aperture
 curl https://raw.githubusercontent.com/jvaswb/patches/refs/heads/sixteen/packages/apps/Aperture/0001-Aperture-Enable-MediaTek-HFPS-Mode-for-60-FPS-video-.patch | git am || {
-  RET=$?
+  RET=1
   git am --abort >/dev/null 2>&1
 }
 cd ../../../
+
 echo "- Applying WPA3 Patch"
 cd external/wpa_supplicant_8
 curl https://raw.githubusercontent.com/jvaswb/patches/refs/heads/sixteen/external/wpa_supplicant_8/do_not_set_NL80211_WPA_VERSION_3.patch | git am || {
-  RET=$?
+  RET=1
   git am --abort >/dev/null 2>&1
 }
 cd ../../
