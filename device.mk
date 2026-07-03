05a9125e7f309196f8a634948933f34230b0cfc5
Cyber Knight <cyberknight755@gmail.com>
2026-07-04 06:50:54 +0700

    X6882: Move to reference fastboot aidl service
    Change-Id: I206d23420952244c40cc73d384c2e8089254f4ae
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: gutssnv <decryptor69@gmail.com>

--- device.mk
+++ device.mk
@@ -176,7 +176,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 
 # FastbootD
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot-service.example_recovery \
     fastbootd
 
 # Bypass Lock State for Fenrir
